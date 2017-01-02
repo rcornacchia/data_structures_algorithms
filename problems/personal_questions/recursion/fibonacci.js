277e6b09a457162eeb162b8442ba252650eacc50
rcornacchia <rlc2160@columbia.edu>
2017-01-02 15:23:31 -0500

    updated fibonacci

--- problems/personal_questions/recursion/fibonacci.js
+++ problems/personal_questions/recursion/fibonacci.js
@@ -1,16 +1,16 @@
 function memoFibonacci(i, memo) {
-    if (i == 0) return memoFibonacci(i, memo[i]==0);
-    if (i == 1) return memoFibonacci(i, memo[i]==1);
-    return memoFibonacci(i-1, memo) + memoFibonacci(i-2, memo);
+    if (i == 0 || i == 1) return i;
+    if (memo[i] == undefined) {
+        memo[i] = memoFibonacci(i-1, memo) + fibonacci(i-2, memo);
+    }
+    return memo[i];
 }
 
 function fibonacci(i) {
-    if (i == 0) return 0;
-    if (i == 1) return 1;
+    if (i == 0 || i == 1) return i;
     return fibonacci(i-1) + fibonacci(i-2);
 }
 
 console.log(fibonacci(5));
-
-
+console.log(memoFibonacci(5, [0, 1]))
 // 0 1 1 2 3 5
