d06ecbc76b6b7ac4be90eeca867b5a70d1b37511
rcornacchia <rlc2160@columbia.edu>
2017-03-28 22:55:46 -0400

    added a palindrome question

---
+++ problems/CrackingtheCodingInterview/chapter_1/4-palindrome.js
@@ -0,0 +1,22 @@
+// input: string
+// output: true if string is palindrome, else false
+
+function isPalindrome(str) {
+    const arr = str.split('');
+    let i = 0;
+    let j = str.length-1;
+    while (i <= j) {
+        if (arr[i] == ' ') i = i + 1;
+        else if (arr[j] == ' ') j = j - 1;
+        else if (arr[i] == arr[j]) {
+            i = i + 1;
+            j = j - 1;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome("taco cat"));
+console.log(isPalindrome("taco cats"));
