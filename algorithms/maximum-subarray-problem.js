acdd5ea3965dc3f1e79e321dc7064b42495bcc23
rcornacchia <rlc2160@columbia.edu>
2017-06-05 21:33:39 -0400

    finished max subarray algorithm

--- algorithms/maximum-subarray-problem.js
+++ algorithms/maximum-subarray-problem.js
@@ -1,7 +1,7 @@
-const arr = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7];
+// const arr = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7];
+const arr = [-23, 18, 20, -7, 12, -5];
 
 const findMaxSubarray = (a, low, high) => {
-  console.log(`finding max subarray with high: ${high}, and low: ${low}`);
   if (high == low) {
     return [low, high, a[low]]; // base case
   } else {
@@ -18,15 +18,13 @@ const findMaxSubarray = (a, low, high) => {
 }
 
 const findMaxCrossingSubarray = (a, low, mid, high) => {
-  console.log(`finding max crossing subarray with high: ${high}, and low: ${low}`);
-
   let maxLeft,
       maxRight,
-      leftSum = -1000000,
-      rightSum = -1000000,
+      leftSum = -Infinity,
+      rightSum = -Infinity,
       sum = 0;
 
-  for (let i = mid; i > low; i--) {
+  for (let i = mid; i >= low; i--) {
     sum = sum + a[i];
     if (sum > leftSum) {
       leftSum = sum;
@@ -34,13 +32,15 @@ const findMaxCrossingSubarray = (a, low, mid, high) => {
     }
   }
 
-  for (let j = mid + 1; j < high; j++) {
+  sum = 0;
+  for (let j = mid + 1; j <= high; j++) {
     sum = sum + a[j];
     if (sum > rightSum) {
       rightSum = sum;
       maxRight = j;
     }
   }
+
   return [maxLeft, maxRight, leftSum + rightSum];
 }
 
